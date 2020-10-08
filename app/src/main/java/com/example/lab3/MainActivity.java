a97c91ec1deb920bcc9d5df1287b991af86f6121
maria <amir0037@algonquinlive.com>
2020-10-08 17:10:57 -0400

    Lab3

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lab3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
@@ -12,16 +13,22 @@ public class MainActivity extends AppCompatActivity {
 
     SharedPreferences prefs;
     EditText email;
+    Button login;
+    String em;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+     //   LoadPreferences();
         email = findViewById(R.id.edittext);
-        prefs = getSharedPreferences("prefs", Context.MODE_PRIVATE);
-//        String savedValue = prefs.getString("Email", "");
-//        email.setText(savedValue);
-//        Button login = findViewById(R.id.button7);
-//        login.setOnClickListener(bt -> saveSharedPreferences(email.getText().toString()));
+        login = findViewById(R.id.button7);
+        Context context = MainActivity.this;
+        prefs = context.getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
+        goToProfile.putExtra("email", email.getText().toString());
+        login.setOnClickListener(bt -> startActivity(goToProfile));
+        goToProfile.putExtra("email", email.getText().toString());
+
 //        LoadPreferences();
     }
 
@@ -29,11 +36,11 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         String savedValue = prefs.getString("Email", "");
         email.setText(savedValue);
-        Button login = findViewById(R.id.button7);
+        login = findViewById(R.id.button7);
         login.setOnClickListener(bt -> saveSharedPreferences(email.getText().toString()));
     }
     private void saveSharedPreferences(String valueToSave) {
-        String em = email.getText().toString();
+        em = email.getText().toString();
         SharedPreferences.Editor editor = prefs.edit();
         editor.putString("Email", em);
         editor.commit();
